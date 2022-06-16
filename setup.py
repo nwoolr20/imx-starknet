0fad48164e59b91cf606e1a7855b7ed02cf324f9
calvwang9 <calvin.wang@immutable.com>
2022-06-16 14:56:13 +1000

    v0.2.0
    Co-authored-by: Zoraiz-Mahmood <35128199+rzmahmood@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='immutablex-starknet',
-    version='0.1.1',
+    version='0.2.0',
     description='Immutable X StarkNet Contracts',
     url='https://github.com/immutable/imx-starknet',
     author='Immutable',
